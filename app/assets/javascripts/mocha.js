a7bc29471bb000429260efa0f911fb1cc1048a81
Yoel Adler <yadler88@gmail.com>
2013-09-09 10:20:24 -0400

    fix heroku issue

--- app/assets/javascripts/mocha.js
+++ app/assets/javascripts/mocha.js
@@ -20,8 +20,8 @@ $(document).ready(function()
 
 	$("#next").click(sift);
 	show(index);
-	setInterval(function(){counterer(); sift();}, 8500);
-//	setInterval(sift, 3000);
+//	setInterval(function(){counterer(); sift();}, 8500);
+	setInterval(sift, 8500);
 	
 	function sift()
 	{
@@ -39,31 +39,4 @@ $(document).ready(function()
 		console.log(scrollPos, "div.tes-"+num);
 	}
 
-	function counterer(x){
-//		var tess = sift(); 
-	var area = $(".tes")[x],
-      results = {
-        paragraphs: document.getElementById('result__paragraphs'),
-        words: document.getElementById('result__words'),
-        characters: document.getElementById('result__characters'),
-        all: document.getElementById('result__all')
-      }
-
-  new Countable.live(area, function (counter) {
-    if ('textContent' in document.body) {
-      results.paragraphs.textContent = counter.paragraphs
-      results.words.textContent = counter.words
-      results.characters.textContent = counter.characters
-      results.all.textContent = counter.all
-    } else {
-      results.paragraphs.innerText = counter.paragraphs
-      results.words.innerText = counter.words
-      results.characters.innerText = counter.characters
-      results.all.textContent = counter.all
-    }
-  })
-	var delay = $("#result__words").text()*250;
-//	sift(delay);
-		test1.value = delay;
-}
 });
