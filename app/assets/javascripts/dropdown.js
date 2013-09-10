c3df36f8a82fa9af624f411f7b284af5f117ce9d
Yoel Adler <yadler88@gmail.com>
2013-09-09 22:50:04 -0400

    fixed faq without js

--- app/assets/javascripts/dropdown.js
+++ app/assets/javascripts/dropdown.js
@@ -1,3 +1,18 @@
+$(function() {
+   tog();
+});
+$(document).on('page:load', function()
+{
+	tog();
+} );
+function tog(){
+var elements = $(".shownbar");
+
+	elements.attr("class", "hiddenbar");
+
+}
+
+
 function togglebar (barID) {
 var whichpost = document.getElementById(barID);
 if (whichpost.className=="shownbar")
