27ab17c8b33e896d2a18ca23f0962ce0dd4d4807
Yoel Adler <yadler88@gmail.com>
2013-09-10 13:35:30 -0400

    close fallfaqs besides for current

--- app/assets/javascripts/dropdown.js
+++ app/assets/javascripts/dropdown.js
@@ -17,5 +17,8 @@ function togglebar (barID) {
 var whichpost = document.getElementById(barID);
 if (whichpost.className=="shownbar")
 { whichpost.className="hiddenbar"; }
-else { whichpost.className="shownbar"; }
+else { 
+	tog();
+	whichpost.className="shownbar";
+	}
 }
